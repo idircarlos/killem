1bb1cbf058e991529e2b45b0f897496d9baff039
Idir Carlos Aliane Crespo <ic.aliane@alumnos.upm.es>
2023-03-27 16:59:55 +0200

    Exec and resolution changed

---
+++ exec.bat
@@ -0,0 +1,19 @@
+@echo off
+set arg1=%1
+if "%arg1%" == "" set arg1=-p
+if "%arg1%" == "-p" python main.py
+if "%arg1%" == "--player" python main.py
+
+if "%arg1%" == "-a" (
+	if "%CONDA_DEFAULT_ENV%" == "%CONDA_DEFAULT_ENV%" ( 
+		activate pygame_env
+	)
+	python agent.py
+)
+
+if "%arg1%" == "--agent" (
+	if "%CONDA_DEFAULT_ENV%" == "%CONDA_DEFAULT_ENV%" ( 
+		activate pygame_env
+	)
+	python agent.py
+)
